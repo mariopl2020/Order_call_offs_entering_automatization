a2156229297f29f024c8eab1ab032fbb9ca3bc72
Mario2 <94698816+mariopl2020@users.noreply.github.com>
2022-04-10 18:21:34 +0200

    Added loop what iterates on every single row with order from call off dataframe and calls appropriate methods. First created method is to gain order as Series object and order number

--- call_off.py
+++ call_off.py
@@ -9,10 +9,22 @@ class CallOff():
 		self.filename = r"calloff_template.xlsx"
 		self.file_processor = CallOffFileProcessing()
 		self.call_off_data_frame = None
+		self.current_order = None
+		self.current_order_no = None
 
 	def get_data_frame(self):
 		"""Getting completely processed dataframe starting from raw xlsx file"""
 
 		self.call_off_data_frame = self.file_processor.give_complete_dataframe(self.filename)
+
+	def give_single_order(self, row_index):
+		"""Gives complete row from dataframe as Series object and order number
+
+		Args:
+			row_index (int): number of row index in dataframe"""
+
+		self.current_order = self.call_off_data_frame.iloc[row_index]
+		self.current_order_no = self.call_off_data_frame.iloc[row_index]["Customer_PO_number"]
+
 
 # @TODO ABSTRACT CLASS for call_off and orders
--- main.py
+++ main.py
@@ -11,15 +11,21 @@ class Program():
 		self.call_off = CallOff()
 		self.files_processing = FilesProcessing()
 
+	def main_loop(self):
+		"""Iterates on single order line from call off and calls main methods to process it"""
+
+		for index in range(len(self.call_off.call_off_data_frame["Customer_PO_number"])):
+			self.call_off.give_single_order(index)
+
 	def main_run(self):
 		"""Collects all key method calls to run program completely"""
 
 		self.call_off.get_data_frame()
 		self.files_processing.write_dataframe_to_excel(self.call_off.call_off_data_frame)
-		# self.files_processing.create_dataframe_from_file()
 
 
 if __name__ == "__main__":
 	program = Program()
 	program.main_run()
+	program.main_loop()
 
